87998b5d10024ba91a0f6ffa63c78e3dc51bcfb6
caio_ <caio_quimica2013@hotmail.com>
2023-07-13 22:05:46 -0300

    GasAPP Atualização com adição de SplashScreen

--- app/src/main/java/com/example/gasapp/MainActivity.kt
+++ app/src/main/java/com/example/gasapp/MainActivity.kt
@@ -1,47 +1,22 @@
 package com.example.gasapp
 
-import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import org.w3c.dom.Text
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var editAlcool: EditText
-    private lateinit var editGasolina: EditText
-    private lateinit var button2: Button
-    private lateinit var textResultado: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editAlcool = findViewById(R.id.editAlcool)
-        editGasolina = findViewById(R.id.editGasolina)
-        button2 = findViewById(R.id.button2)
-        textResultado = findViewById(R.id.textResultado)
+        supportActionBar?.hide()
 
-        button2.setOnClickListener {
-            val precoAlcool = editAlcool.text.toString().toDouble()
-            val precoGasolina = editGasolina.text.toString().toDouble()
-
-            val resultado = compararPrecos(precoAlcool, precoGasolina)
-
-            textResultado.text = resultado
-        }
-    }
-
-    private fun compararPrecos(precoAlcool: Double, precoGasolina: Double): String {
-
-    val proporcao = precoAlcool / precoGasolina
-
-    return if (proporcao < 0.7) {
-        "Compensa abastecer com Álcool!"
-    } else {
-        "Compensa abastecer com Gasolina!"
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
-}
-    
-    
